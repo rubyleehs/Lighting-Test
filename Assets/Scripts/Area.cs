19e2ffce8b958f036184ec23647b11abed07328b
rubyleehs <rubyleehs@live.com>
2019-03-02 17:06:06 +0800

    Dungeon Connections

--- Assets/Scripts/Area.cs
+++ Assets/Scripts/Area.cs
@@ -11,51 +11,55 @@ public class Area : MonoBehaviour
     public int minSubAreaSize = 6;
     public int maxSplitVariance = 5;
 
-    public int[,] tiles;
+    public int[,] tiles; //-1 wall, 0 unchecked, 1 in room, 2 corridor, 3 connection
     public SubArea mainSubArea;
 
-
-    // Start is called before the first frame update
     void Start()
+    {
+        GenerateArea();
+    }
+
+    void GenerateArea()
     {
         tiles = new int[areaSize.x, areaSize.y];
         mainSubArea = new SubArea
         {
-            size = new Vector2Int(areaSize.x -4, areaSize.y -4),
+            size = new Vector2Int(areaSize.x - 4, areaSize.y - 4),
             anchor = Vector2Int.one * 2,
         };
         MaxSplit(mainSubArea);
         Place(mainSubArea);
         Replace(1, 0);
         PlaceAll(mainSubArea);
-        Replace(0, 2);
+        Replace(0, -1);
+        ConnectAll(mainSubArea);
         Show();
     }
     
     void MaxSplit(SubArea a0)
     {
-        if(a0.childSubAreas == null)
+        if(a0.childsSubArea == null)
         {
             a0.SplitArea(maxSplitVariance, minSubAreaSize);
 
-            for (int i = 0; i < a0.childSubAreas.Count; i++)
+            for (int i = 0; i < a0.childsSubArea.Count; i++)
             {
-                MaxSplit(a0.childSubAreas[i]);
+                MaxSplit(a0.childsSubArea[i]);
             }
         }
     }
 
     void PlaceAll(SubArea a0)
     {
-        if (a0.childSubAreas.Count == 0)
+        if (a0.childsSubArea.Count == 0)
         {
             Place(a0);
         }
         else
         {
-            for (int i = 0; i < a0.childSubAreas.Count; i++)
+            for (int i = 0; i < a0.childsSubArea.Count; i++)
             {
-                PlaceAll(a0.childSubAreas[i]);
+                PlaceAll(a0.childsSubArea[i]);
             }
         }
 
@@ -67,8 +71,15 @@ public class Area : MonoBehaviour
         {
             for (int dx = -1; dx < a0.size.x +1; dx++)
             {
-                if((dx == -1) || (dy == -1) || (dx == a0.size.x) || (dy == a0.size.y)) tiles[a0.anchor.x + dx, a0.anchor.y + dy] = -1;
-                else tiles[a0.anchor.x + dx, a0.anchor.y + dy] = 1;
+                if (a0.isTooSmall)
+                {
+                    tiles[a0.anchor.x + dx, a0.anchor.y + dy] = 2;
+                }
+                else
+                {
+                    if ((dx == -1) || (dy == -1) || (dx == a0.size.x) || (dy == a0.size.y)) tiles[a0.anchor.x + dx, a0.anchor.y + dy] = -1;
+                    else tiles[a0.anchor.x + dx, a0.anchor.y + dy] = 1;
+                }
             }
         }
     }
@@ -94,52 +105,90 @@ public class Area : MonoBehaviour
             }
         }
     }
+
+    void ConnectAll(SubArea a0)
+    {
+        for (int i = 0; i < a0.childsSubArea.Count; i++)
+        {
+            ConnectAll(a0.childsSubArea[i]);
+        }
+        Connect(a0);
+    }
+
+    void Connect(SubArea a)
+    {
+        a.connection = new List<Vector2Int>();
+        if (a.childsSubArea.Count != 2) return;
+        SubArea a0 = a.childsSubArea[0];
+        SubArea a1 = a.childsSubArea[1];
+        int dy = -1;
+        int dx = -1;
+
+        if (a0.anchor.y == a1.anchor.y) dy = Random.Range(2, a1.size.y -1);
+        else dx = Random.Range(2, a1.size.x -1);
+        a.connection.Add(new Vector2Int(a1.anchor.x + dx, a1.anchor.y + dy));
+
+        while (true)
+        {
+            if (a0.anchor.y == a1.anchor.y) dx--;
+            else dy--;
+
+            if (tiles[a1.anchor.x + dx, a1.anchor.y + dy] > 0) break;
+            else
+            {
+                a.connection.Add(new Vector2Int(a1.anchor.x + dx, a1.anchor.y + dy));
+            }
+        }
+
+        for (int i = 0; i < a.connection.Count; i++)
+        {
+            tiles[a.connection[i].x, a.connection[i].y] = 3;
+        }
+    }
 }
 
 [System.Serializable]
 public class SubArea
 {
     public SubArea parentSubArea;
-    public List<SubArea> childSubAreas;
+    public List<SubArea> childsSubArea;
+    public List<Vector2Int> connection;
     public Vector2Int anchor;
     public Vector2Int size;
+    public bool isTooSmall = false;
 
     public void SplitArea(int maxSplitVariance, int minSubAreaSize)
     {
+        childsSubArea = new List<SubArea>();
+        if (size.x < 2* minSubAreaSize || size.y < 2 * minSubAreaSize)
+        {
+            if(size.x < minSubAreaSize || size.y < minSubAreaSize) isTooSmall = true;
+            return;
+        }
+
         bool splitX = Random.Range(0, size.x + size.y) > size.y;
         int variance = Random.Range(-maxSplitVariance, maxSplitVariance + 1);
-        childSubAreas = new List<SubArea>
-        {
-            new SubArea(), new SubArea(),
-        };
 
-        childSubAreas[0].parentSubArea = this;
-        childSubAreas[1].parentSubArea = this;
+        childsSubArea.Add(new SubArea());
+        childsSubArea.Add(new SubArea());
+        childsSubArea[0].parentSubArea = this;
+        childsSubArea[1].parentSubArea = this;
 
         if (splitX)
         {
-            childSubAreas[0].size = new Vector2Int(Mathf.FloorToInt(size.x * 0.5f) + variance - 1, size.y);
-            childSubAreas[0].anchor = anchor;
+            childsSubArea[0].size = new Vector2Int(Mathf.FloorToInt(size.x * 0.5f) + variance - 1, size.y);
+            childsSubArea[0].anchor = anchor;
 
-            childSubAreas[1].size = new Vector2Int(Mathf.FloorToInt(size.x * 0.5f) - variance - 1, size.y);
-            childSubAreas[1].anchor = anchor + Vector2Int.right * (childSubAreas[0].size.x + 2);
+            childsSubArea[1].size = new Vector2Int(Mathf.FloorToInt(size.x * 0.5f) - variance - 1, size.y);
+            childsSubArea[1].anchor = anchor + Vector2Int.right * (childsSubArea[0].size.x + 2);
         }
         else
         {
-            childSubAreas[0].size = new Vector2Int(size.x, Mathf.FloorToInt(size.y * 0.5f) + variance - 1);
-            childSubAreas[0].anchor = anchor;
+            childsSubArea[0].size = new Vector2Int(size.x, Mathf.FloorToInt(size.y * 0.5f) + variance - 1);
+            childsSubArea[0].anchor = anchor;
 
-            childSubAreas[1].size = new Vector2Int(size.x, Mathf.FloorToInt(size.y * 0.5f) - variance - 1);
-            childSubAreas[1].anchor = anchor + Vector2Int.up * (childSubAreas[0].size.y + 2);
-        }
-
-        for (int i = 0; i < childSubAreas.Count; i++)
-        {
-            if (childSubAreas[i].size.x < minSubAreaSize || childSubAreas[i].size.y < minSubAreaSize)
-            {
-                childSubAreas.RemoveAt(i);
-                i--;
-            }
+            childsSubArea[1].size = new Vector2Int(size.x, Mathf.FloorToInt(size.y * 0.5f) - variance - 1);
+            childsSubArea[1].anchor = anchor + Vector2Int.up * (childsSubArea[0].size.y + 2);
         }
     }
 }
